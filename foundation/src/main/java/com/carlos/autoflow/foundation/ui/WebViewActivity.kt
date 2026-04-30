12073515dd2b2ab34522a695aba96093e0948fce
carlos <363465939@qq.com>
2026-04-30 21:24:36 +0800

    增加日志

--- foundation/src/main/java/com/carlos/autoflow/foundation/ui/WebViewActivity.kt
+++ foundation/src/main/java/com/carlos/autoflow/foundation/ui/WebViewActivity.kt
@@ -2,8 +2,13 @@ package com.carlos.autoflow.foundation.ui
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.webkit.WebResourceError
+import android.webkit.WebResourceRequest
+import android.webkit.WebResourceResponse
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
@@ -22,8 +27,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import com.carlos.autoflow.foundation.BuildConfig
 import com.carlos.autoflow.foundation.ui.components.FoundationTopAppBar
 import com.carlos.autoflow.foundation.web.FoundationWebViewHelper
+import com.carlos.autoflow.utils.AutoFlowLogger
 
 class WebViewActivity : BaseComposeActivity() {
     companion object {
@@ -68,6 +75,49 @@ class WebViewActivity : BaseComposeActivity() {
                             WebView(context).apply {
                                 webViewHelper.value = FoundationWebViewHelper(this).also { helper ->
                                     helper.configure(
+                                        client = object : WebViewClient() {
+                                            override fun onPageStarted(
+                                                view: WebView?,
+                                                url: String?,
+                                                favicon: Bitmap?
+                                            ) {
+                                                if (BuildConfig.DEBUG) {
+                                                    AutoFlowLogger.d("WebViewActivity", "页面开始加载: $url")
+                                                }
+                                            }
+
+                                            override fun onPageFinished(view: WebView?, url: String?) {
+                                                if (BuildConfig.DEBUG) {
+                                                    AutoFlowLogger.d("WebViewActivity", "页面加载完成: $url")
+                                                }
+                                            }
+
+                                            override fun onReceivedError(
+                                                view: WebView?,
+                                                request: WebResourceRequest?,
+                                                error: WebResourceError?
+                                            ) {
+                                                if (BuildConfig.DEBUG && request?.isForMainFrame == true) {
+                                                    AutoFlowLogger.e(
+                                                        "WebViewActivity",
+                                                        "页面加载失败: ${request.url} code=${error?.errorCode} desc=${error?.description}"
+                                                    )
+                                                }
+                                            }
+
+                                            override fun onReceivedHttpError(
+                                                view: WebView?,
+                                                request: WebResourceRequest?,
+                                                errorResponse: WebResourceResponse?
+                                            ) {
+                                                if (BuildConfig.DEBUG && request?.isForMainFrame == true) {
+                                                    AutoFlowLogger.e(
+                                                        "WebViewActivity",
+                                                        "页面HTTP错误: ${request.url} status=${errorResponse?.statusCode}"
+                                                    )
+                                                }
+                                            }
+                                        },
                                         chromeClient = object : android.webkit.WebChromeClient() {
                                             override fun onProgressChanged(view: WebView?, newProgress: Int) {
                                                 progress = newProgress
